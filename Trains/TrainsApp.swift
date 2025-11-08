5d1b2371b76f501ab248f99fc60095e2127cbb2a
Bogdan Kalitenko <kalitenkobv@gmail.com>
2025-11-08 17:41:15 +0500

    Initial Commit

---
+++ Trains/TrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainsApp.swift
+//  Trains
+//
+//  Created by Bogdan Kalitenko on 08.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
