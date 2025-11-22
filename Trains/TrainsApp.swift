22ee858dd77e1b263d320c97bbd47ab9c66de65b
Bogdan Kalitenko <kalitenkobv@gmail.com>
2025-11-22 17:38:43 +0500

    chore: rename colors and add LaunchScreenView
    Sprint 19

--- Trains/TrainsApp.swift
+++ Trains/TrainsApp.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 @main
-struct TrainsApp: App {
+struct TrainsApp: App {    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LaunchScreenView()
         }
     }
 }
